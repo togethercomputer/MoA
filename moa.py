0bb4bc8789fdf46efda3b8881d2e2015684c8b56
Zhichao Li <zhichao@together.ai>
2025-01-06 17:36:19 -0800

    updated the MoA recipe to be current

--- moa.py
+++ moa.py
@@ -8,12 +8,12 @@ client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
 async_client = AsyncTogether(api_key=os.environ.get("TOGETHER_API_KEY"))
 user_prompt = "What are 3 fun things to do in SF?"
 reference_models = [
-    "Qwen/Qwen2-72B-Instruct",
-    "Qwen/Qwen1.5-72B-Chat",
-    "mistralai/Mixtral-8x22B-Instruct-v0.1",
-    "databricks/dbrx-instruct",
+    "meta-llama/Llama-3.3-70B-Instruct-Turbo",
+    "Qwen/Qwen2.5-72B-Instruct-Turbo",
+    "Qwen/Qwen2.5-Coder-32B-Instruct",
+    "microsoft/WizardLM-2-8x22B"
 ]
-aggregator_model = "mistralai/Mixtral-8x22B-Instruct-v0.1"
+aggregator_model = "Qwen/Qwen2.5-72B-Instruct-Turbo"
 aggreagator_system_prompt = """You have been provided with a set of responses from various open-source models to the latest user query. Your task is to synthesize these responses into a single, high-quality response. It is crucial to critically evaluate the information provided in these responses, recognizing that some of it may be biased or incorrect. Your response should not simply replicate the given answers but should offer a refined, accurate, and comprehensive reply to the instruction. Ensure your response is well-structured, coherent, and adheres to the highest standards of accuracy and reliability.
 
 Responses from models:"""
